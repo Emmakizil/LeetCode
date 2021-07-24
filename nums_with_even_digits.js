81cde9e2e44b0744af8d6b8c1a77da2c5be5ce27
Fereshteh <fmodabber@outlook.com>
2021-07-23 19:15:48 -0700

    string length

---
+++ nums_with_even_digits.js
@@ -0,0 +1,14 @@
+const findNumbers = function(nums) {
+    let count = 0;
+    for (i=0; i<nums.length; i++){
+       string = nums[i].toString().length;
+       if(string % 2 == 0) count++   
+    }
+    return count;
+};
+
+console.log(findNumbers([112,12,345,1111]))
+
+/* What did I learn:
+    - how to get length of each item: convert it to a string
+*/
