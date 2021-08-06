f071fb9ce9ae4dec4c6201036b1f675426062d9f
Fereshteh <fmodabber@outlook.com>
2021-08-06 10:42:15 -0700

    join method

--- shuffle_string.js
+++ shuffle_string.js
@@ -3,16 +3,20 @@ Given a string s and an integer array indices of the same length.
 The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 Return the shuffled string.*/
 
-var restoreString = function(s, indices) {
-    let sIndices = []
-        
-    for (let i=0; i<indices.length; i++) {
-        sIndices[indices[i]] = s[i]
+const restoreString = function(s, indices) {
+    let indiceOfS = [];
+    for(i=0; i<indices.length; i++){
+        indiceOfS[indices[i]] = s[i]
     }
-    return sIndices
+    return indiceOfS.join('')
 };
+
 
 // var restoreString = function(s, indices) {
 //     return s.split('').map((index) => s[indices.indexOf(index)]).join('')
 // };
 console.log(restoreString('herlo', [3,4,1,2,0]))
+
+/* What did I learn:
+ -reviwed join method
+ */
